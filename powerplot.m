bcccc2777dfefae497e0f63c2504f35224fd9a51
SnowzTail <i@snowztail.com>
2018-10-09 10:15:26 +0100

    Plot w.r.t. subchannel value

--- powerplot.m
+++ powerplot.m
@@ -5,6 +5,11 @@ lagrangeMultiplier = 1;
 filterTaps = [0.1+0.1i, 0.2+0.8i, 0.01+0.2i, 1+1i, 3+1i, 0.1+0.7i, 0.09+0.02i, 0.1+0.8i, 0.4+0.8i, 0.1+0.3i];
 
 %% Calculate and plot carriers power
-carrierPower = waterfilling(nSubcarriers, noiseLevel, lagrangeMultiplier, filterTaps);
-
-bar(carrierPower);
+carrierPower = waterfilling(noiseLevel, lagrangeMultiplier, filterTaps);
+carrierNoise = abs(filterTaps) .^2 / noiseLevel;
+carrierStatus = [carrierNoise; carrierPower];
+bar(carrierStatus', 'stacked');
+title('Optimal power allocations w.r.t. subchannel noise');
+xlabel('Carrier index')
+ylabel('Power')
+legend('Noise','Power')
